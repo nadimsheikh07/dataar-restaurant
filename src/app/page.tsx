01c79cfbe73dc5521db62ea4b10e223ed2a60944
Nadim Sheikh <nadim.sheikh.07@gmail.com>
2025-07-21 15:02:45 +0530

    add image list

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,14 @@
 "use client";
 
+import MenuImageList from "@/components/imageList";
 import { Box, Typography } from "@mui/material";
 
 export default function Home() {
   return (
     <Box>
-        <Typography>Home Page</Typography>
+      <Typography>Home Page</Typography>
+
+      <MenuImageList />
     </Box>
   );
 }
